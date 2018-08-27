09c7fbd39929d99c425472bec41fa9e5ef207082
donLoron <lw@walderwerber.ch>
2018-08-27 10:53:28 +0200

    added copy to clipboard, direct addon opener

--- assets/js/linkpicker.js
+++ assets/js/linkpicker.js
@@ -27,7 +27,6 @@ if(returnButtons.length > 0) {
 
   // Get the element, add a click listener...
   document.getElementsByTagName("body")[0].addEventListener("click", function(e) {
-    console.log(e);
     if(e.target && e.target.classList.contains(addonName + "-return")) {
       if(confirm("Möchten Sie die URL [" + e.target.getAttribute("data-href") + "] kopieren")) {
         eraseCookie(addonName);
@@ -74,4 +73,34 @@ function readCookie(name) {
 // Erase cookie
 function eraseCookie(name) {
   createCookie(name,"",-1);
+}
+
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Fallback: Copying text command was ' + msg);
+  } catch (err) {
+    console.error('Fallback: Oops, unable to copy', err);
+  }
+
+  document.body.removeChild(textArea);
+}
+
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(function() {
+    console.log('Async: Copying to clipboard was successful!');
+  }, function(err) {
+    console.error('Async: Could not copy text: ', err);
+  });
 }
---
+++ pages/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: walderwerber
+ * Date: 27.08.18
+ * Time: 10:27
+ */
